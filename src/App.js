30da6059ea6f23055df1b48aa66266f06c70c685
magmaslime <7324172+magmaslime@users.noreply.github.com>
2020-09-08 07:25:20 +0900

    Use drag test app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ConnectedReduxTest, rootReducer, initialState } from './ReduxTest'
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
+import DragTest from './DragTest'
 
-function App() {
+
+// Store creation
+
+const store = createStore(rootReducer, initialState);
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      {/* <ConnectedReduxTest /> */}
+      <DragTest />
+    </Provider>
   );
 }
 
-export default App;
+
